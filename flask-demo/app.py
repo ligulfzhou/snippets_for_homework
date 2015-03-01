7a8a0fff611975af0537afbdfc390fc2bb12047a
Ligulf Zhou <donsol53@gmail.com>
2015-03-01 22:06:25 +0800

    flask demo

--- flask-demo/app.py
+++ flask-demo/app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    hello_world = "hello world!"
+    return render_template('index.html', hello_world = hello_world)
+
+if __name__ == "__main__":
+    app.run()
+
